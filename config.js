c945ff7ba50db671b100bcefe52ddb6afabd50a7
André Baptista <andre.m.b.1@hotmail.com>
2021-03-06 15:24:48 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MONGO_URI: 'mongodb://batistacenas:BolyVWfdaz1HODY7bn9rpKz3RomrhamfOTZ5rLOc8JtNhlj6nRjTIKTTTqKPvO5GcXK8CufOvjM4Jepc0fF8sg==@batistacenas.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@batistacenas@'
+    MONGO_URI: 'mongodb://batistacenas:BolyVWfdaz1HODY7bn9rpKz3RomrhamfOTZ5rLOc8JtNhlj6nRjTIKTTTqKPvO5GcXK8CufOvjM4Jepc0fF8sg==@batistacenas.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@batistacenas@'
 };
